c44a439d4848046142de7076808e2d1deda3243d
cramination <camnation@comcast.net>
2025-03-30 18:45:13 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,77 @@ function switchVideos() {
         return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
     }
 });
+
+function showButton() {
+    document.getElementById('img-full-1')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+
+function showButton2() {
+    document.getElementById('img-full-2')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+
+function showButton3() {
+    document.getElementById('img-full-3')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+
+function showButton4() {
+    document.getElementById('img-full-4')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+
+function showButton5() {
+    document.getElementById('img-full-5')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+function showButton6() {
+    document.getElementById('img-full-6')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+function showButton7() {
+    document.getElementById('img-full-7')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+function showButton8() {
+    document.getElementById('img-full-8')
+        .style.visibility = "visible";
+    document.getElementById('hide-button')
+        .style.display = "block";
+}
+
+function hideButton() {
+    document.getElementById('img-full-1')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-2')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-3')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-4')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-5')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-6')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-7')
+        .style.visibility = "hidden";
+    document.getElementById('img-full-8')
+        .style.visibility = "hidden";
+    document.getElementById('hide-button')
+    .style.display = "none";
+}
